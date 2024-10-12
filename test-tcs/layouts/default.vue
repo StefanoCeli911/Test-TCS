7ffed279e008f620c7b43ffe1097243a4e05ed28
Stefano Celi <stefano.celi91@gmail.com>
2024-10-12 15:14:26 +0200

    Add animation to mobile menu

--- test-tcs/layouts/default.vue
+++ test-tcs/layouts/default.vue
@@ -1,7 +1,5 @@
 <template>
-
-  <div class="min-h-screen flex flex-col bg-gray-100">
-
+  <div class="min-h-screen flex flex-col bg-gray-100 relative">
     <!-- Navbar -->
     <header class="bg-gray-800 text-white p-4">
       <nav class="container mx-auto flex justify-between items-center">
@@ -10,7 +8,7 @@
          <!-- Icona hamburger mobile -->
          <div class="lg:hidden">
           <button @click="toggleMenu" class="focus:outline-none">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <svg xmlns="http://www.w3.org/2000/svg" :class="{'transform rotate-90': isMenuOpen}" class="h-6 w-6 transition-transform duration-300" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
             </svg>
           </button>
@@ -23,18 +21,21 @@
         </ul>
       </nav>
 
-    <!-- Links mobile -->
-    <div v-if="isMenuOpen" class="lg:hidden">
-        <ul class="flex flex-col space-y-2 mt-4">
-          <li><NuxtLink to="/" class="hover:text-gray-400">Home</NuxtLink></li>
-          <li><NuxtLink to="/login" class="hover:text-gray-400">Login</NuxtLink></li>
-        </ul>
-      </div>
+      <!-- Menu mobile -->
+      <transition name="slide-left">
+        <div v-if="isMenuOpen" class="rounded bold absolute right-0 top-14 w-2/5 bg-gray-800 text-white shadow-lg z-10">
+          <div class="container mx-auto py-4">
+            <ul class="space-y-4">
+              <li class="text-center"><NuxtLink @click="toggleMenu" to="/" class="block text-lg hover:text-slate-400">Home</NuxtLink></li>
+              <li class="text-center"><NuxtLink @click="toggleMenu" to="/login" class="block text-lg hover:text-slate-400">Login</NuxtLink></li>
+            </ul>
+          </div>
+        </div>
+      </transition>
     </header>
 
-
     <!-- Page Content -->
-    <main class="flex-grow container mx-auto">
+    <main class="flex-grow container mx-auto p-4">
       <NuxtPage />
     </main>
 
@@ -50,9 +51,21 @@
 <script setup>
 import { ref } from 'vue'
 
+// Stato del menu
 const isMenuOpen = ref(false)
 
+// Toggle menu
 const toggleMenu = () => {
   isMenuOpen.value = !isMenuOpen.value
 }
 </script>
+
+<style scoped>
+/* Animazione slide-left */
+.slide-left-enter-active, .slide-left-leave-active {
+  transition: transform 0.3s ease;
+}
+.slide-left-enter-from, .slide-left-leave-to {
+  transform: translateX(+100%);
+}
+</style>
